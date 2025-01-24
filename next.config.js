314a7bf8d972b7236799e96f3c734296f6977660
developerNd <91623104+developerNd@users.noreply.github.com>
2025-01-24 12:32:16 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,21 @@ const nextConfig = {
   images: {
     domains: ['leadbazaar-na2mnamow-developernds-projects.vercel.app'], // Replace 'localhost' with your actual domains
   },
-  
+  experimental: {
+    serverActions: true,
+    typedRoutes: true,
+  },
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@': '.',
+      },
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig 
