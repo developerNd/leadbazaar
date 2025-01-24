60624a696b8732dd26484ac6ffa27b007b2b6e47
developerNd <91623104+developerNd@users.noreply.github.com>
2025-01-24 12:05:28 +0530

    again updated next config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['leadbazaar-na2mnamow-developernds-projects.vercel.app'], // Replace 'localhost' with your actual domains
   },
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname),
-      },
-    };
-    return config;
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig 
