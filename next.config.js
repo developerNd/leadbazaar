f2f30bc31953beb1205092752d9b4cc7b41f05ff
developerNd <91623104+developerNd@users.noreply.github.com>
2025-01-24 12:23:02 +0530

    .

--- next.config.js
+++ next.config.js
@@ -10,9 +10,7 @@ const nextConfig = {
   images: {
     domains: ['leadbazaar-na2mnamow-developernds-projects.vercel.app'], // Replace 'localhost' with your actual domains
   },
-  experimental: {
-    appDir: true,
-  },
+  
 }
 
 module.exports = nextConfig 
